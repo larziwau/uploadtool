20026c32ac6bcdf6502e96d35f7a8a5ad7a9edf6
larzie <lunar@lavache.com>
2024-12-25 15:51:42 +0300

    Update upload script

--- upload.sh
+++ upload.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-set +x # Do not leak information
+set +x
 
-# Exit immediately if one of the files given as arguments is not there
-# because we don't want to delete the existing release if we don't have
-# the new files that should be uploaded 
 for file in "$@"
 do
     if [ ! -e "$file" ]
@@ -20,7 +17,7 @@ fi
 if command -v sha256sum >/dev/null 2>&1 ; then
   shatool="sha256sum"
 elif command -v shasum >/dev/null 2>&1 ; then
-  shatool="shasum -a 256" # macOS fallback
+  shatool="shasum -a 256"
 else
   echo "Neither sha256sum nor shasum is available, cannot check hashes"
 fi
@@ -47,11 +44,6 @@ if [ ! -z "$UPLOADTOOL_BODY" ] ; then
   RELEASE_BODY="$UPLOADTOOL_BODY"
 fi
 
-# The calling script (usually .travis.yml) can set a suffix to be used for
-# the tag and release name. This way it is possible to have a release for
-# the output of the CI/CD pipeline (marked as 'continuous') and also test
-# builds for other branches.
-# If this build was triggered by a tag, call the result a Release
 if [ ! -z "$UPLOADTOOL_SUFFIX" ] ; then
   if [ "$UPLOADTOOL_SUFFIX" = "$GIT_TAG" ] ; then
     RELEASE_NAME="$GIT_TAG"
@@ -65,13 +57,10 @@ if [ ! -z "$UPLOADTOOL_SUFFIX" ] ; then
     else
       is_prerelease="true"
     fi
-
   fi
 else
-  # ,, is a bash-ism to convert variable to lower case
   case $(tr '[:upper:]' '[:lower:]' <<< "$GIT_TAG") in
     "")
-      # Do not use "latest" as it is reserved by GitHub
       RELEASE_NAME="continuous"
       RELEASE_TITLE="Continuous build"
       if [ -z "$UPLOADTOOL_ISPRERELEASE" ] ; then
@@ -103,8 +92,6 @@ if [ "$ARTIFACTORY_BASE_URL" != "" ]; then
 
   files="$@"
 
-  # artifactory doesn't support any kind of "artifact description", so we're uploading a text file containing the
-  # relevant details along with the other artifacts
   tempdir=$(mktemp -d)
   info_file="$tempdir"/build-info.txt
   echo "Travis CI build log: ${TRAVIS_BUILD_WEB_URL}" > "$info_file"
@@ -134,8 +121,6 @@ if [ "$ARTIFACTORY_BASE_URL" != "" ]; then
   rm -r "$tempdir"
 fi
 
-# Do not upload non-master branch builds
-# if [ "$GIT_TAG" != "$TRAVIS_BRANCH" ] && [ "$TRAVIS_BRANCH" != "master" ]; then export TRAVIS_EVENT_TYPE=pull_request; fi
 if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ] || [ "$GITHUB_EVENT_NAME" == "pull_request" ] ; then
   echo "Release uploading disabled for pull requests"
   if [ "$ARTIFACTORY_BASE_URL" != "" ]; then
@@ -147,24 +132,10 @@ if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ] || [ "$GITHUB_EVENT_NAME" == "pull
     for FILE in "$@" ; do
       BASENAME="$(basename "${FILE}")"
       curl --upload-file $FILE "https://transfersh.com/$BASENAME" > ./one-upload
-      echo "$(cat ./one-upload)" # this way we get a newline
-      echo -n "$(cat ./one-upload)\\n" >> ./uploaded-to # this way we get a \n but no newline
+      echo "$(cat ./one-upload)"
+      echo -n "$(cat ./one-upload)\\n" >> ./uploaded-to
     done
   fi
-#  review_url="https://api.github.com/repos/${GIT_REPO_SLUG}/pulls/${TRAVIS_PULL_REQUEST}/reviews"
-#  if [ -z $UPLOADTOOL_PR_BODY ] ; then
-#    body="Travis CI has created build artifacts for this PR here:"
-#  else
-#    body="$UPLOADTOOL_PR_BODY"
-#  fi
-#  body="$body\n$(cat ./uploaded-to)\nThe link(s) will expire 14 days from now."
-#  review_comment=$(curl -X POST \
-#    --header "Authorization: token ${GITHUB_TOKEN}" \
-#    --data '{"commit_id": "'"$GIT_COMMIT"'","body": "'"$body"'","event": "COMMENT"}' \
-#    $review_url)
-#  if echo $review_comment | grep -q "Bad credentials" 2>/dev/null ; then
-#    echo '"Bad credentials" response for --data {"commit_id": "'"$GIT_COMMIT"'","body": "'"$body"'","event": "COMMENT"}'
-#  fi
   $shatool "$@"
   exit 0
 fi
@@ -197,80 +168,46 @@ fi
 
 tag_url="https://api.github.com/repos/$GIT_REPO_SLUG/git/refs/tags/$RELEASE_NAME"
 tag_infos=$(curl -XGET --header "Authorization: token ${GITHUB_TOKEN}" "${tag_url}")
-echo "tag_infos: $tag_infos"
 tag_sha=$(echo "$tag_infos" | grep '"sha":' | head -n 1 | cut -d '"' -f 4 | cut -d '{' -f 1)
-echo "tag_sha: $tag_sha"
 
 release_url="https://api.github.com/repos/$GIT_REPO_SLUG/releases/tags/$RELEASE_NAME"
-echo "Getting the release ID..."
-echo "release_url: $release_url"
 release_infos=$(curl -XGET --header "Authorization: token ${GITHUB_TOKEN}" "${release_url}")
-echo "release_infos: $release_infos"
 release_id=$(echo "$release_infos" | grep "\"id\":" | head -n 1 | tr -s " " | cut -f 3 -d" " | cut -f 1 -d ",")
-echo "release ID: $release_id"
 upload_url=$(echo "$release_infos" | grep '"upload_url":' | head -n 1 | cut -d '"' -f 4 | cut -d '{' -f 1)
-echo "upload_url: $upload_url"
 release_url=$(echo "$release_infos" | grep '"url":' | head -n 1 | cut -d '"' -f 4 | cut -d '{' -f 1)
-echo "release_url: $release_url"
 target_commit_sha=$(echo "$release_infos" | grep '"target_commitish":' | head -n 1 | cut -d '"' -f 4 | cut -d '{' -f 1)
-echo "target_commit_sha: $target_commit_sha"
 
 if [ "$GIT_COMMIT" != "$target_commit_sha" ] ; then
-
   echo "GIT_COMMIT != target_commit_sha, hence deleting $RELEASE_NAME..."
   
   if [ ! -z "$release_id" ]; then
     delete_url="https://api.github.com/repos/$GIT_REPO_SLUG/releases/$release_id"
-    echo "Delete the release..."
-    echo "delete_url: $delete_url"
-    curl -XDELETE \
-        --header "Authorization: token ${GITHUB_TOKEN}" \
-        "${delete_url}"
+    curl -XDELETE --header "Authorization: token ${GITHUB_TOKEN}" "${delete_url}"
   fi
-
-  # echo "Checking if release with the same name is still there..."
-  # echo "release_url: $release_url"
-  # curl -XGET --header "Authorization: token ${GITHUB_TOKEN}" \
-  #     "$release_url"
 
   if [ "$RELEASE_NAME" == "continuous" ] ; then
-    # if this is a continuous build tag, then delete the old tag
-    # in preparation for the new release
-    echo "Delete the tag..."
     delete_url="https://api.github.com/repos/$GIT_REPO_SLUG/git/refs/tags/$RELEASE_NAME"
-    echo "delete_url: $delete_url"
-    curl -XDELETE \
-        --header "Authorization: token ${GITHUB_TOKEN}" \
-        "${delete_url}"
+    curl -XDELETE --header "Authorization: token ${GITHUB_TOKEN}" "${delete_url}"
   fi
-
-  echo "Create release..."
 
   release_infos=$(curl -H "Authorization: token ${GITHUB_TOKEN}" \
        --data '{"tag_name": "'"$RELEASE_NAME"'","target_commitish": "'"$GIT_COMMIT"'","name": "'"$RELEASE_TITLE"'","body": "'"$RELEASE_BODY"'","draft": false,"prerelease": '$is_prerelease'}' "https://api.github.com/repos/$GIT_REPO_SLUG/releases")
 
-  echo "$release_infos"
-
   unset upload_url
   upload_url=$(echo "$release_infos" | grep '"upload_url":' | head -n 1 | cut -d '"' -f 4 | cut -d '{' -f 1)
-  echo "upload_url: $upload_url"
 
   unset release_url
   release_url=$(echo "$release_infos" | grep '"url":' | head -n 1 | cut -d '"' -f 4 | cut -d '{' -f 1)
-  echo "release_url: $release_url"
-
-fi # if [ "$GIT_COMMIT" != "$tag_sha" ]
+fi
 
 if [ -z "$release_url" ] ; then
-	echo "Cannot figure out the release URL for $RELEASE_NAME"
-	exit 1
+  echo "Cannot figure out the release URL for $RELEASE_NAME"
+  exit 1
 fi
 
 echo "Upload binaries to the release..."
 
-# Need to URL encode the basename, so we have this function to do so
 urlencode() {
-  # urlencode <string>
   old_lc_collate=$LC_COLLATE
   LC_COLLATE=C
   local length="${#1}"
@@ -298,10 +235,7 @@ done
 $shatool "$@"
 
 if [ "$GIT_COMMIT" != "$tag_sha" ] ; then
-  echo "Publish the release..."
-
   release_infos=$(curl -H "Authorization: token ${GITHUB_TOKEN}" \
        --data '{"draft": false}' "$release_url")
-
   echo "$release_infos"
-fi # if [ "$GIT_COMMIT" != "$tag_sha" ]
+fi
